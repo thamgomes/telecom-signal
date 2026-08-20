919d972a02e802e41a3b0bb7000d7d02075b5b8b
Thais <thaismachado.1979@gmail.com>
2026-08-20 10:33:46 -0300

    fix: servir telecom-news.html na rota raiz

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'telecom-news.html')));
 
 // Agente HTTPS sem verificacao de cert (para Anatel/Fundamentus)
 const agentInsecure = new https.Agent({ rejectUnauthorized: false });
